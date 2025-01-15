b85ec8e615c2c380a6183ba3ebb47bff888e8412
yacodev <cyaco33@gmail.com>
2025-01-14 22:23:30 -0500

    add new table: products-images and  seed

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   // Add the following import entity
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
