3e20fb2633a1e133d6a46b10c1f5a3d9438db674
yacodev <cyaco33@gmail.com>
2025-01-19 19:39:32 -0500

    impement websocket

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,69 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { MessagesWsService } from './messages-ws.service';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message-dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() wss: Server;
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+  handleDisconnect(client: Socket) {
+    //console.log('Client disconnected', client.id);
+    this.messagesWsService.removeClient(client.id);
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto): void {
+    // emit to only client
+    // client.emit('message-from-server', {
+    //   fullName: 'John Doe',
+    //   message: payload.message || 'Hello, World!',
+    // });
+
+    // emit to all clients except the sender
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'John Doe',
+    //   message: payload.message || 'Hello, World!',
+    // });
+
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'Hello, World!',
+    });
+  }
+}
